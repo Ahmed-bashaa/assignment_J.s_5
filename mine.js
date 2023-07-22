12aa4987761e05baffb51da8a3d534fb0412cc06
Ahmed-bashaa <hoda472000@gmail.com>
2023-07-22 16:17:20 +0300

    first commit

--- mine.js
+++ mine.js
@@ -1,87 +1,55 @@
+// document.addEventListener("mousemove",function(einfo){
+// document.querySelector("img").style.left=einfo.clientX+"px";
+// document.querySelector("img").style.top=einfo.clientY+"px";
 
-var NameInput = document.getElementById("NameId") ;  // tag
-var UrlInput = document.getElementById("URLId") ;  // tag
-var cartoona=[] ; 
+// })
 
-if(  localStorage.getItem("local") == null )
-{
-    cartoona = [] ;
-}
-else
-{
-      cartoona =  JSON.parse(localStorage.getItem("local")) ;
-      display()
-}
+// var allItames= Array.from(document.querySelectorAll("img"))
 
-var regex_Name = /^\w{3,}(\s+\w+)*$/
-var regex_Url = /^(https?:\/\/)?(w{3}\.)?\w+\.\w{2,}\/?(:\d{2,5})?(\/\w+)*$/;
-function add_Date()
-{
-    if( regex_Name.test(NameInput.value) && regex_Url.test(UrlInput.value))
-    {
-        
-        var Bookmark_opject = {
+// console.log(allItames)
+// for (var i = 0; i < allItames.length; i++) {
 
-            Name : NameInput.value ,
-            Url: UrlInput.value ,
-        }
-        
-        cartoona.push(Bookmark_opject);
     
-        // localstorage
-        localStorage.setItem("local" ,  JSON.stringify(cartoona)  )
-    
-        // display
-        display()
-    
-        // clear el inputs
-        clearInputs()
-        
-    }
-    else
-    {
-        swal(
-             "Site Name or Url is not valid, Please follow the rules below :"  ,               
-               `-->Site name must contain at least 3 characters
+// allItames[i].addEventListener("click", function(einfo){
 
-              --> Site URL must be a valid one
-               ` ,
-               "error"
-            );    
-    }
+// var imgSrs=einfo.target.src;
+
+
+// console.log(imgSrs)
+// document.querySelector(".minDiv").style.backgroundImage=`url(${imgSrs})`
+
+
+
+// })
+
+
+
+// }
+
+var allItames = Array.from(document.querySelectorAll("img"));
+var layerParent = document.querySelector(".layerParent");
+for (var i = 0; i < allItames.length; i++) {
+
+    allItames[i].addEventListener("click" ,function () {
+
+        layerParent.classList.replace("d-none","d-flex")
+
+
+
+    })
+
+
+
 }
 
 
-function clearInputs()
-{
-    NameInput.value = null;
-    UrlInput.value = null;
-}
 
-function display()
-{    
-    var hasalah = ``;
-    for(var  i =1 ; i < cartoona.length ; i++)
-    {
-        //cartoona[i] ====> tr
-      hasalah +=  
-      `<tr>
-            <td>${i}</td>
-            <td>${cartoona[i].Name}</td>
-            <td>
-             <a class="btn btn-outline-primary" href="${cartoona[i].Url}" target="_blank"><i class="fa-solid fa-eye" style="color: #424242;"></i> Visit</a>
-            </td>
-            <td>
-             <button onclick="Delete_btn(${i})" class="btn btn-outline-danger"><i class="fa-solid fa-trash" style="color: #484d56;"></i> Delete</button>
-            </td>
-      </tr>`
-    }
-    document.getElementById("tBody").innerHTML = hasalah;
-}
 
-function Delete_btn(pIndex)
-{
-    cartoona.splice( pIndex , 1 ) 
-    localStorage.setItem("local" , JSON.stringify(  cartoona ))
-    display() 
-}
+
+
+
+
+
+
+
+
